34c399ceb1f3334c33aeb8e557b9b2a88eae73a0
kpehl <30782087+kpehl@users.noreply.github.com>
2020-08-13 06:19:22 -0400

    changed script to swap between two style sheets; updated html; modified style sheets to match script

--- assets/js/change-color-scheme.js
+++ assets/js/change-color-scheme.js
@@ -1,10 +1,9 @@
-document.getElementById("change-scheme-button").addEventListener('click',function() {
-    let redschemeEnabled = document.body.classList.toggle('red-scheme');
-    localStorage.setItem('redschemeEnabled', redschemeEnabled);
-});
-
-if (JSON.parse(localStorage.getItem('redschemeEnabled'))) {
-    document.body.classList.add('red-scheme');
-    document.header.classList.add('red-scheme');
-    document.h2.classList.add('red-scheme')
-}
+function redstyle() {
+    var theme = document.getElementById('theme');
+    theme.href = "./assets/css/redstyle.css";
+  }
+  
+  function bluestyle() {
+    var theme = document.getElementById('theme');
+    theme.href = "./assets/css/bluestyle.css";
+  }
